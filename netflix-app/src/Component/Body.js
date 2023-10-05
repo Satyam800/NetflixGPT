1067238a74418ef95c2472825863b2f462c632f6
Satyam chandra <satyamchandra7277@gmail.com>
2023-10-05 23:09:12 +0530

    refactor code of redirect of authentication page according to user is signIn/SignUp

--- netflix-app/src/Component/Body.js
+++ netflix-app/src/Component/Body.js
@@ -6,13 +6,11 @@ import {
   createBrowserRouter,
   useNavigate,
 } from "react-router-dom";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { auth } from "../Utils/firebase_config";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../Utils/Slice/userSlice";
+
+
 
 const Body = () => {
-  const dispatch = useDispatch();
+  
 
   const appRouter = createBrowserRouter([
     {
@@ -25,30 +23,7 @@ const Body = () => {
     },
   ]);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-
-        const { uid, displayName, email, photoURL } = user;
-        dispatch(
-          addUser({
-            uid: uid,
-            displayName: displayName,
-            email: email,
-            photoURL: photoURL,
-          })
-        );
-
-        // ...
-      } else {
-        // User is signed out
-        // ...
-        dispatch(removeUser());
-      }
-    });
-  }, []);
+ 
   return (
     <>
       <RouterProvider router={appRouter} />
---
+++ netflix-app/src/Utils/constant.js
@@ -0,0 +1,9 @@
+export const Netflix_logo="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+export const LoginPage_backgrd="https://assets.nflxext.com/ffe/siteui/vlv3/f85718e8-fc6d-4954-bca0-f5eaf78e0842/ea44b42b-ba19-4f35-ad27-45090e34a897/IN-en-20230918-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+export const API_options= {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNWZlZmUyNzVhYWU3Y2U0MWRjOGVhOWRiZTg2ZTA3OSIsInN1YiI6IjYzZTBlM2MwODc0MWM0MDA5OWI1NWRiYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6WGCQf3e-YU-701l-n9HMez-eZC8adbEob3t7ABkE7k'
+    }
+  };
