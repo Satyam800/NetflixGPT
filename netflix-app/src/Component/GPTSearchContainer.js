2f626ee8ea869847f83fe037165a90636cc8e4d7
Satyam chandra <satyamchandra7277@gmail.com>
2024-01-22 10:48:45 +0530

    refactor the search functionalities

--- netflix-app/src/Component/GPTSearchContainer.js
+++ netflix-app/src/Component/GPTSearchContainer.js
@@ -1,57 +1,63 @@
-import React,{useEffect, useRef} from 'react'
-import { BrowsePage_BG, IMG_CDN_URL } from '../Utils/constant'
-import lang from "../Utils/languageSelect"
-import { useSelector,useDispatch } from 'react-redux'
-import { API_call } from '../Utils/Slice/GPTSlice'
-import openai from '../Utils/OpenAI'
-import { Movie_fetch } from '../Utils/Slice/GPTSlice'
-import GptmovieSuggs from './GptmovieSuggs'
-import MovieCard from './MovieCard'
+import React, { useEffect, useRef, useState } from "react";
+import { BrowsePage_BG, IMG_CDN_URL } from "../Utils/constant";
+import lang from "../Utils/languageSelect";
+import { useSelector, useDispatch } from "react-redux";
+import { API_call } from "../Utils/Slice/GPTSlice";
+import openai from "../Utils/OpenAI";
+import { Movie_fetch } from "../Utils/Slice/GPTSlice";
+import GptmovieSuggs from "./GptmovieSuggs";
+import MovieCard from "./MovieCard";
+import { ColorRing } from "react-loader-spinner";
 
 const GPTSearchContainer = () => {
-  const dispatch=useDispatch()
-  const SearchText=useRef(null)
-  const toggelLang=useSelector(store=>store.lang.languageChange)
-  const TMDB_movie=useSelector((store)=>store.GPT.movie_array)
-  const TMDB_SearchResult=useSelector(store=>store.GPT.final_result)
- 
-  const handleOpenAISearch=()=>{   
-    dispatch(API_call(SearchText))  
-   dispatch(Movie_fetch(TMDB_movie))
-   
-    }
-    
- console.log("rensered");
+  const dispatch = useDispatch();
+  const SearchText = useRef(null);
+  const toggelLang = useSelector((store) => store.lang.languageChange);
+  const TMDB_movie = useSelector((store) => store.GPT.movie_array);
+  const TMDB_SearchResult = useSelector((store) => store.GPT.final_result);
+  const [loader, Setloader] = useState(false);
+  console.log(TMDB_movie, "TTMDB");
 
+  const handleOpenAISearch = () => {
+    Setloader(true);
+    dispatch(API_call(SearchText));
+  };
+  useEffect(() => {
+    dispatch(Movie_fetch(TMDB_movie));
+    Setloader(false);
+  }, [TMDB_movie]);
   return (
-   <>
-    <div className=" fixed top-0 -z-10">
-      <img className=" w-screen sm:h-[70%]    "
-      alt="Img"
-      src={BrowsePage_BG}
-      />
+    <>
+      <div className=" fixed top-0 -z-10">
+        <img
+          className=" w-screen sm:h-[70%]    "
+          alt="Img"
+          src={BrowsePage_BG}
+        />
 
- <div className=" flex absolute justify-between top-16 left-[20%] w-[50%] mt-[10%] h-14 bg-zinc-400 p-2 shadow-lg">
- <input className=" text-xl font-semibold align-baseline w-[82%] focus:outline-none focus:ring focus:border-blue-500 -500 h-[90%] p-2 mt-1 rounded-sm  " 
-  placeholder={lang[toggelLang]?.GetSearchplaceholder}
-  ref={SearchText}
- />
- 
-<div className='font-semibold text-xl w-28 text-center  bg-black  pt-1 align-center rounded-md text-red-600 cursor-pointer'
-onClick={handleOpenAISearch}
->
-{lang[toggelLang]?.search}
-</div>
-  </div>  
-</div>
+        <div className=" flex absolute justify-between top-16 left-[20%] w-[50%] mt-[10%] h-14 bg-zinc-400 p-2 shadow-lg">
+          <input
+            className=" text-xl font-semibold align-baseline w-[82%] focus:outline-none focus:ring focus:border-blue-500 -500 h-[90%] p-2 mt-1 rounded-sm  "
+            placeholder={lang[toggelLang]?.GetSearchplaceholder}
+            ref={SearchText}
+          />
 
+          <div
+            className="font-semibold text-xl w-28 text-center  bg-black  pt-1 align-center rounded-md text-red-600 cursor-pointer"
+            onClick={handleOpenAISearch}
+          >
+            {!loader ? (
+              lang[toggelLang]?.search
+            ) : (
+              <ColorRing className="ml-[30%]" height="30" width="30" />
+            )}
+          </div>
+        </div>
+      </div>
 
-{TMDB_SearchResult?.length ? <GptmovieSuggs/>:null}
+      {TMDB_SearchResult?.length ? <GptmovieSuggs /> : null}
+    </>
+  );
+};
 
- 
-   </>
-  )
-}
-
-
-export default GPTSearchContainer
+export default GPTSearchContainer;
--- netflix-app/src/Component/GptmovieSuggs.js
+++ netflix-app/src/Component/GptmovieSuggs.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MovieCard from './MovieCard'
 import { useSelector } from 'react-redux'
 const GptmovieSuggs = () => {
     const TMDB_SearchResult=useSelector(store=>store.GPT.final_result)
+    
   return (
-   <>
-  
+   <>  
    <div className=' fixed overflow-y-scroll  mt-[20%] bg-gradient-to-r from-black from-10% via-sky-200 via-30% to-orange-100 to-90% opacity-80 m-4 p-3'>
   {
     <div className='  flex flex-wrap '>
 
-{TMDB_SearchResult?.map(i=><MovieCard img={i?.poster_path} title={i.title} key={i.id}/>)}
+{TMDB_SearchResult?.map(i=><MovieCard img={i?.poster_path} title={i.title} key={i.id} />)}
     </div>
   }
+
 </div>
-   
+
    </>
   )
 }
