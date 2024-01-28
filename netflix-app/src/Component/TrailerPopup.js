e18d8852b58835bba9b6455ce4513d13ec7dfaf8
Satyam chandra <satyamchandra7277@gmail.com>
2024-01-29 00:11:51 +0530

    array optional chaining

--- netflix-app/src/Component/TrailerPopup.js
+++ netflix-app/src/Component/TrailerPopup.js
@@ -3,14 +3,15 @@ import React from 'react'
 const TrailerPopup = ({data}) => {
   
     const item=data?.filter((i)=>i.type==="Trailer")
-if(item[0]==undefined) return
+    console.log(item,"item");
+if(item?.[0]==undefined) return
   return (
    <>
    <div className='w-full h-full '>
    <iframe
         className="  w-full h-full"
         src={
-           `https://www.youtube.com/embed/${item[0]?.key}`+"?&autoplay=1&mute=1"
+           `https://www.youtube.com/embed/${item?.[0]?.key}`+"?&autoplay=1&mute=1"
         }
       ></iframe>  
    </div>
